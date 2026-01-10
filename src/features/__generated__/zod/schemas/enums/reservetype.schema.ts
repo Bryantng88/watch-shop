589479d1476ed1ede1e0850d3fcfc5fdedfe32d2
ND-Long <118421077+tx-long@users.noreply.github.com>
2026-01-10 14:36:48 +0700

    uppp

--- src/features/__generated__/zod/schemas/enums/reservetype.schema.ts
+++ src/features/__generated__/zod/schemas/enums/reservetype.schema.ts
@@ -1,5 +1,5 @@
 import * as z from 'zod';
 
-export const ReserveTypeSchema = z.enum(['NONE', 'COD_HOLD', 'DEPOSIT_HOLD'])
+export const ReserveTypeSchema = z.enum(['NONE', 'COD', 'DEPOSIT'])
 
 export type ReserveType = z.infer<typeof ReserveTypeSchema>;
